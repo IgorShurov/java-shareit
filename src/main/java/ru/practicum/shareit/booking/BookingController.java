cfef8192ed55169fa8d773c85b657117bea171ae
IgorShurov <igor.shurof@yandex.ru>
2024-03-12 20:03:53 +0300

    code style fix

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -29,47 +29,37 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping()
-    public BookingOutDto addBookings(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId, @RequestBody @Valid BookingDto bookingDto){
+    public BookingOutDto addBookings(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId, @RequestBody @Valid BookingDto bookingDto) {
         log.info("POST: request to the endpoint was received: '/bookings' user {}, add new booking {}", userId, "bookingDto.getName()");
         if (bookingDto.getStart() == null || bookingDto.getEnd() == null) {
             throw new ValidationException("Booking: Dates are null!");
         }
-        if (bookingDto.getEnd().isBefore(bookingDto.getStart()) || bookingDto.getStart().isEqual(bookingDto.getEnd())
-                || bookingDto.getEnd().isBefore(LocalDateTime.now()) || bookingDto.getStart().isBefore(LocalDateTime.now())) {
+        if (bookingDto.getEnd().isBefore(bookingDto.getStart()) || bookingDto.getStart().isEqual(bookingDto.getEnd()) || bookingDto.getEnd().isBefore(LocalDateTime.now()) || bookingDto.getStart().isBefore(LocalDateTime.now())) {
             throw new ValidationException("Booking: Problem in dates");
         }
         return bookingService.addBooking(bookingDto, userId);
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingOutDto confirmationBooking(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId,
-                                             @PathVariable @Min(1) Long bookingId,
-                                             @RequestParam(value = "approved") Boolean approved) {
+    public BookingOutDto confirmationBooking(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId, @PathVariable @Min(1) Long bookingId, @RequestParam(value = "approved") Boolean approved) {
         log.info("PATCH: request to the endpoint was received: '/bookings' user {}, changed the status booking {}", userId, bookingId);
         return bookingService.confirmationBooking(userId, bookingId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public BookingOutDto getBookingById(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId,
-                                        @PathVariable @Min(1) Long bookingId) {
+    public BookingOutDto getBookingById(@RequestHeader(Constants.HEADER_USER_ID) @Min(1) Long userId, @PathVariable @Min(1) Long bookingId) {
 
         log.info("GET: request to the endpoint was received: '/bookings' get booking {}", bookingId);
         return bookingService.getBookingByIdAndBookerId(userId, bookingId);
     }
 
     @GetMapping
-    public List<BookingOutDto> getAllBrookingByBookerId(@RequestHeader(HEADER_USER_ID) @Min(1) Long userId,
-                                                        @RequestParam(defaultValue = "ALL") String state,
-                                                        @RequestParam(value = "from", defaultValue = "0") @Min(0) Integer from,
-                                                        @RequestParam(value = "size", defaultValue = "10") @Min(1) @Max(20) Integer size) {
+    public List<BookingOutDto> getAllBrookingByBookerId(@RequestHeader(HEADER_USER_ID) @Min(1) Long userId, @RequestParam(defaultValue = "ALL") String state, @RequestParam(value = "from", defaultValue = "0") @Min(0) Integer from, @RequestParam(value = "size", defaultValue = "10") @Min(1) @Max(20) Integer size) {
         return bookingService.getAllBrookingByBookerId(PageRequest.of(from / size, size), userId, state);
     }
 
     @GetMapping("owner")
-    public List<BookingOutDto> getAllBookingsForAllItemsByOwnerId(@RequestHeader(HEADER_USER_ID) @Min(1) Long userId,
-                                                                  @RequestParam(defaultValue = "ALL") String state,
-                                                                  @RequestParam(value = "from", defaultValue = "0") @Min(0) Integer from,
-                                                                  @RequestParam(value = "size", defaultValue = "10") @Min(1) @Max(20) Integer size) {
+    public List<BookingOutDto> getAllBookingsForAllItemsByOwnerId(@RequestHeader(HEADER_USER_ID) @Min(1) Long userId, @RequestParam(defaultValue = "ALL") String state, @RequestParam(value = "from", defaultValue = "0") @Min(0) Integer from, @RequestParam(value = "size", defaultValue = "10") @Min(1) @Max(20) Integer size) {
         return bookingService.getAllBookingsForAllItemsByOwnerId(PageRequest.of(from / size, size), userId, state);
     }
 }
